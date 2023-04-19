772cf3a645b32b6c979ae41e353c01f0d272c2f2
victor25g81 <100184175+victor25g81@users.noreply.github.com>
2023-04-19 08:55:52 -0600

    Clase que representa una tarea
    Co-Authored-By: John Ortiz Ordoñez <johnortizo@outlook.com>

---
+++ src/tarea.js
@@ -0,0 +1,8 @@
+class Tarea {
+    constructor(contenido, fecha, hora){
+        this.terminado = false;
+        this.contenido = contenido;
+        this.fecha = fecha;
+        this.hora = hora;
+    }
+}
