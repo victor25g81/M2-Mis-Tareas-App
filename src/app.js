b95f305b9ddb59b7ec7ae25ccc6439543481cc89
victor25g81 <100184175+victor25g81@users.noreply.github.com>
2023-04-28 09:02:41 -0600

    Validación de los campos para crear una tarea

--- src/app.js
+++ src/app.js
@@ -1,38 +1,32 @@
 $(document).ready(() => {
-    
+  document
+    .querySelector("#modalFechaHora")
+    .addEventListener("show.bs.modal", function () {
+      const fechaHoraActual = moment();
 
-    document.querySelector('#modalFechaHora').addEventListener('show.bs.modal', function() {
-        const fechaHoraActual = moment();
-        
-        document.querySelector('#fecha').value = fechaHoraActual.format('YYYY-MM-DD');
-        document.querySelector("#fecha").min = fechaHoraActual.format('YYYY-MM-DD');
-        
-        fechaHoraActual.hours(fechaHoraActual.hours() + 1);
-        fechaHoraActual.minutes(0);
-        document.querySelector('#hora').value = fechaHoraActual.format('HH:mm');
+      document.querySelector("#fecha").value =
+        fechaHoraActual.format("YYYY-MM-DD");
+      document.querySelector("#fecha").min =
+        fechaHoraActual.format("YYYY-MM-DD");
+
+      fechaHoraActual.add(1, "m");
+      document.querySelector("#hora").min = fechaHoraActual.format("HH:mm");
+
+      fechaHoraActual.hours(fechaHoraActual.hours() + 1);
+      fechaHoraActual.minutes(0);
+      document.querySelector("#hora").value = fechaHoraActual.format("HH:mm");
     });
-});
 
-//var horaseleccionada = document.querySelector('#hora').value = fechaHoraActual.format('HH:mm');
-
-horaseleccionada.addEventListener("change", function(){
-
-    document.querySelector('#hora').value = fechaHoraActual.format('HH:mm');
-
-    if(horaseleccionada.isBefore(hora)){
-
-        alert("la hora y minutos no pueden ser inferiores a la hora actual");
-
-        this.value = fechaHoraActual.format("HH:mm");
-    }
 
 });
 
+function checkInput() {
+  var input = document.getElementById("contenido").value.trim();
+  var button = document.getElementById("crearTarea");
 
-function validar() {
-    var contenido = document.getElementById("contenido").value;
-    if (contenido === "") {
-      alert("Por favor, ingrese su nombre");
-      addEventListener.preventDefault(); // evita que se envíe el formulario
-    }
-  };
+  if (input.length > 0) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+}
